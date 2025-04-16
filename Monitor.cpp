a6c605659f431941fdad2ba8b605ae0de903f57e
ff28ea <ff28ea@foxmail.com>
2025-04-16 20:17:54 +0800

    init

---
+++ Monitor.cpp
@@ -0,0 +1,273 @@
+﻿#include <iostream>
+#define NOMINMAX
+#include <windows.h>
+#include <vector>
+#include <dwmapi.h>
+#include <format>
+
+#include <d3d11.h>
+#pragma comment(lib, "d3d11.lib")
+
+#include "ImGui/imgui.h"
+#include "ImGui/imgui_internal.h"
+#include "ImGui/imgui_impl_dx11.h"
+#include "ImGui/imgui_impl_win32.h"
+
+struct Monitor {
+    int  Width;
+    int  Height;
+    int  Left;
+    int  Top;
+};
+
+std::vector<Monitor> Monitors;
+int CurrentMonitors = 0;
+
+int itMonitors = 0;
+int MaxMonitorWidth = 0, MaxMonitorHeight = 0;
+
+BOOL CALLBACK MonitorEnumProc(HMONITOR hMonitor, HDC hdcMonitor, LPRECT lprcMonitor, LPARAM dwData) {
+    MONITORINFOEX mi{};
+    mi.cbSize = sizeof(MONITORINFOEX);
+    if (GetMonitorInfo(hMonitor, &mi)) {
+        int Width = mi.rcMonitor.right - mi.rcMonitor.left;
+        int Height = mi.rcMonitor.bottom - mi.rcMonitor.top;
+        if (mi.dwFlags & MONITORINFOF_PRIMARY)
+            CurrentMonitors = itMonitors;
+
+        MaxMonitorWidth = std::max(Width, MaxMonitorWidth);
+        MaxMonitorHeight = std::max(Height, MaxMonitorHeight);
+        Monitors.push_back(
+            { 
+                Width,
+                Height, 
+                mi.rcMonitor.left,
+                mi.rcMonitor.top
+            }
+        );
+        itMonitors ++;
+    }
+    return TRUE;
+}
+
+
+static ID3D11Device* g_pd3dDevice = nullptr;
+static ID3D11DeviceContext* g_pd3dDeviceContext = nullptr;
+static IDXGISwapChain* g_pSwapChain = nullptr;
+static ID3D11RenderTargetView* g_mainRenderTargetView = nullptr;
+static UINT                     g_ResizeWidth = 0, g_ResizeHeight = 0;
+
+extern IMGUI_IMPL_API LRESULT ImGui_ImplWin32_WndProcHandler(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
+
+// Win32 message handler
+// You can read the io.WantCaptureMouse, io.WantCaptureKeyboard flags to tell if dear imgui wants to use your inputs.
+// - When io.WantCaptureMouse is true, do not dispatch mouse input data to your main application, or clear/overwrite your copy of the mouse data.
+// - When io.WantCaptureKeyboard is true, do not dispatch keyboard input data to your main application, or clear/overwrite your copy of the keyboard data.
+// Generally you may always pass all inputs to dear imgui, and hide them from your application based on those two flags.
+LRESULT WINAPI WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+    if (ImGui_ImplWin32_WndProcHandler(hWnd, msg, wParam, lParam))
+        return true;
+
+    switch (msg)
+    {
+    case WM_SIZE:
+        if (wParam == SIZE_MINIMIZED)
+            return 0;
+        g_ResizeWidth = (UINT)LOWORD(lParam); // Queue resize
+        g_ResizeHeight = (UINT)HIWORD(lParam);
+        return 0;
+    case WM_SYSCOMMAND:
+        if ((wParam & 0xfff0) == SC_KEYMENU) // Disable ALT application menu
+            return 0;
+        break;
+    case WM_DESTROY:
+        ::PostQuitMessage(0);
+        return 0;
+    case WM_DPICHANGED:
+        if (ImGui::GetIO().ConfigFlags & 16384)
+        {
+            //const int dpi = HIWORD(wParam);
+            //printf("WM_DPICHANGED to %d (%.0f%%)\n", dpi, (float)dpi / 96.0f * 100.0f);
+            const RECT* suggested_rect = (RECT*)lParam;
+            ::SetWindowPos(hWnd, nullptr, suggested_rect->left, suggested_rect->top, suggested_rect->right - suggested_rect->left, suggested_rect->bottom - suggested_rect->top, SWP_NOZORDER | SWP_NOACTIVATE);
+        }
+        break;
+    }
+    return ::DefWindowProcW(hWnd, msg, wParam, lParam);
+}
+
+bool CreateDeviceD3D(HWND hWnd);
+void CleanupDeviceD3D();
+void CreateRenderTarget();
+void CleanupRenderTarget();
+
+
+
+
+int main()
+{
+    EnumDisplayMonitors(NULL, NULL, MonitorEnumProc, 0);
+
+    WNDCLASSEXW wc;
+    memset(&wc, 0, sizeof(WNDCLASSEXW));
+    wc.cbSize = sizeof(WNDCLASSEXW);
+    wc.style = NULL;
+    wc.lpfnWndProc = WndProc;
+    wc.cbClsExtra = NULL;
+    wc.cbWndExtra = NULL;
+    wc.hInstance = NULL;
+    wc.hIcon = NULL;
+    wc.hCursor = NULL;
+    wc.hbrBackground = (HBRUSH)(RGB(0, 0, 0));
+    wc.lpszClassName = L"CEF-OSC-WIDGET";
+    wc.hIconSm = NULL;
+    RegisterClassExW(&wc);
+    HWND hwnd = CreateWindowEx((WS_EX_TOOLWINDOW | WS_EX_TRANSPARENT | WS_EX_TOPMOST | WS_EX_LAYERED),
+        wc.lpszClassName, L"NVIDIA GeForce Overlay", WS_POPUP,
+        0, 0, MaxMonitorWidth, MaxMonitorHeight, 0, 0, 0, 0);
+
+    SetWindowLongA(hwnd, GWL_EXSTYLE, GetWindowLong(hwnd, GWL_EXSTYLE) | WS_EX_LAYERED);
+    SetLayeredWindowAttributes(hwnd, RGB(0, 0, 0), 255, 2);
+    MARGINS margins = { -1 };
+    ::DwmExtendFrameIntoClientArea(hwnd, &margins);
+    ::ShowWindow(hwnd, SW_SHOWDEFAULT);
+    ::UpdateWindow(hwnd);
+
+    if (!CreateDeviceD3D(hwnd)) {
+        CleanupDeviceD3D();
+        UnregisterClassW(wc.lpszClassName, wc.hInstance);
+        return 0;
+    }
+
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO(); (void)io;
+    ImFontConfig cfg;
+    ImFontConfig cfg_regular;
+ //   io.DisplaySize = ImVec2((float)Width, (float)Height);
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+
+    ImGui::StyleColorsDark();
+
+    ImGui_ImplWin32_Init(hwnd);
+    ImGui_ImplDX11_Init(g_pd3dDevice,g_pd3dDeviceContext);
+
+    bool done = true;
+    while (done)
+    {
+        MSG msg;
+        while (::PeekMessage(&msg, nullptr, 0U, 0U, PM_REMOVE))
+        {
+            ::TranslateMessage(&msg);
+            ::DispatchMessage(&msg);
+            if (msg.message == WM_QUIT)
+                done = false;
+        }
+        SetWindowPos(hwnd, (HWND)-1, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE);
+        SetWindowLong(hwnd, GWL_EXSTYLE, WS_EX_TOOLWINDOW | WS_EX_TRANSPARENT);
+
+
+        ImGui_ImplDX11_NewFrame();
+        ImGui_ImplWin32_NewFrame();
+        ImGui::NewFrame();
+
+        ImGui::Begin("hello");
+
+        static bool is;
+        ImGui::Checkbox("1", &is);
+
+        if (ImGui::Button("exit")) {
+            exit(0);
+        }
+    
+        auto screen_width = Monitors[CurrentMonitors].Width;
+        auto screen_height = Monitors[CurrentMonitors].Height;
+        std::string comboPreview = std::format("{}x{} (Monitor{})", screen_width, screen_width, CurrentMonitors);
+        if (ImGui::BeginCombo("Monitors", comboPreview.c_str()))
+        {
+            for (int i = 0; i < Monitors.size(); i++) {
+                auto& pMonitor = Monitors[i];
+                const bool isSelected = CurrentMonitors == i;
+                auto monitorName = std::format("{}x{} (Monitor{})", pMonitor.Width, pMonitor.Height, i);
+                if (ImGui::Selectable(monitorName.c_str(), isSelected)) {
+                    CurrentMonitors = i;
+                    SetWindowPos(hwnd, 0, pMonitor.Left, pMonitor.Top, 0, 0, SWP_NOZORDER | SWP_NOSIZE);
+                }
+                if (isSelected)
+                    ImGui::SetItemDefaultFocus();
+            }
+            ImGui::EndCombo();
+        }
+        ImGui::End();
+     
+        ImGui::GetBackgroundDrawList()->AddRectFilled({ 0,0 }, { (float)MaxMonitorWidth, (float)MaxMonitorHeight }, ImColor(0, 0, 0));
+
+        const auto clear_color = ImVec4(0.0f, 0.0f, 0.0f, 0.00f);
+        ImGui::Render();
+        g_pd3dDeviceContext->OMSetRenderTargets(1, &g_mainRenderTargetView, NULL);
+        g_pd3dDeviceContext->ClearRenderTargetView(g_mainRenderTargetView, (float*)&clear_color);
+        ImGui_ImplDX11_RenderDrawData(ImGui::GetDrawData());
+        g_pSwapChain->Present(1, 0);
+
+    }
+
+    std::cin.get();
+}
+
+
+
+bool CreateDeviceD3D(HWND hWnd)
+{
+    // Setup swap chain
+    DXGI_SWAP_CHAIN_DESC sd;
+    ZeroMemory(&sd, sizeof(sd));
+    sd.BufferCount = 2;
+    sd.BufferDesc.Width = 0;
+    sd.BufferDesc.Height = 0;
+    sd.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+    sd.BufferDesc.RefreshRate.Numerator = 60;
+    sd.BufferDesc.RefreshRate.Denominator = 1;
+    sd.Flags = DXGI_SWAP_CHAIN_FLAG_ALLOW_MODE_SWITCH;
+    sd.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+    sd.OutputWindow = hWnd;
+    sd.SampleDesc.Count = 1;
+    sd.SampleDesc.Quality = 0;
+    sd.Windowed = TRUE;
+    sd.SwapEffect = DXGI_SWAP_EFFECT_DISCARD;
+
+    UINT createDeviceFlags = 0;
+    //createDeviceFlags |= D3D11_CREATE_DEVICE_DEBUG;
+    D3D_FEATURE_LEVEL featureLevel;
+    const D3D_FEATURE_LEVEL featureLevelArray[2] = { D3D_FEATURE_LEVEL_11_0, D3D_FEATURE_LEVEL_10_0, };
+    HRESULT res = D3D11CreateDeviceAndSwapChain(nullptr, D3D_DRIVER_TYPE_HARDWARE, nullptr, createDeviceFlags, featureLevelArray, 2, D3D11_SDK_VERSION, &sd, &g_pSwapChain, &g_pd3dDevice, &featureLevel, &g_pd3dDeviceContext);
+    if (res == DXGI_ERROR_UNSUPPORTED) // Try high-performance WARP software driver if hardware is not available.
+        res = D3D11CreateDeviceAndSwapChain(nullptr, D3D_DRIVER_TYPE_WARP, nullptr, createDeviceFlags, featureLevelArray, 2, D3D11_SDK_VERSION, &sd, &g_pSwapChain, &g_pd3dDevice, &featureLevel, &g_pd3dDeviceContext);
+    if (res != S_OK)
+        return false;
+
+    CreateRenderTarget();
+    return true;
+}
+
+void CleanupDeviceD3D()
+{
+    CleanupRenderTarget();
+    if (g_pSwapChain) { g_pSwapChain->Release(); g_pSwapChain = nullptr; }
+    if (g_pd3dDeviceContext) { g_pd3dDeviceContext->Release(); g_pd3dDeviceContext = nullptr; }
+    if (g_pd3dDevice) { g_pd3dDevice->Release(); g_pd3dDevice = nullptr; }
+}
+
+void CreateRenderTarget()
+{
+    ID3D11Texture2D* pBackBuffer;
+    g_pSwapChain->GetBuffer(0, IID_PPV_ARGS(&pBackBuffer));
+    g_pd3dDevice->CreateRenderTargetView(pBackBuffer, nullptr, &g_mainRenderTargetView);
+    pBackBuffer->Release();
+}
+
+void CleanupRenderTarget()
+{
+    if (g_mainRenderTargetView) { g_mainRenderTargetView->Release(); g_mainRenderTargetView = nullptr; }
+}
